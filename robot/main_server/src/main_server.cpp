dc10a26e2c90737762b8e9afc794375bdb5c4d57
Micha <mlk04@wp.pl>
2019-11-23 14:08:00 +0100

    initial commit

--- robot/main_server/src/main_server.cpp
+++ robot/main_server/src/main_server.cpp
@@ -1,3 +1,6 @@
+#include "VL53L1X.h"
+#include "LSM6DS3.h"
+#include "filter.h"
 #include "bcm2835.h"
 #include "l6470constants.h"
 #include "motors.h"
@@ -39,16 +42,21 @@ using boost::asio::ip::udp;
 #define GPIO_TOF_10 RPI_V2_GPIO_P1_37
 #define NDEBUG
 
+void tofTest();
+void IMUtest();
 void stepperTest();
 Motors *globalBoard;
+VL53L1X *globalSensors[10];
 uint16_t measurement[10];
+std::ofstream file;
 
 void sigintHandler(int signum) {
 	if (signum == SIGINT) {
 		//globalBoard->Dump();
-		globalBoard->stop();
-		std::cout<<"TASK TERMINATED"<<endl;
-		exit(signum);
+		if (file.is_open()) file.close();
+		if (globalBoard != nullptr) globalBoard->stop();
+		for(int i = 0; i < 10; i++)
+			if (globalSensors[i] != nullptr) globalSensors[i]->disable();
 	}
 }
 
@@ -61,6 +69,8 @@ int main()
 			fprintf(stderr, "Not able to init the bmc2835 library\n");
 			return -1;
 	}
+
+	/*
 	Motors board( BCM2835_SPI_CS0, GPIO_RESET_OUT);
 	globalBoard = &board;
 	board.setUp();
@@ -154,6 +164,10 @@ int main()
 		//clipCapture.release();
 		//break;
 	}
+	*/
+
+	tofTest();
+	IMUtest();
 
   return 0;
 }
@@ -173,4 +187,247 @@ void stepperTest(){
 	positionRight = board.getPositionRight();
 	printf("Absolute position: Left:%lu		Right:%lu \n",positionLeft, positionRight);
 	board.stop();
+}
+
+void tofTest(){
+
+	// Log file
+	file.open("distance_log_report70cm");
+
+	bcm2835_gpio_fsel(GPIO_TOF_1, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(GPIO_TOF_2, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(GPIO_TOF_3, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(GPIO_TOF_4, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(GPIO_TOF_5, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(GPIO_TOF_6, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(GPIO_TOF_7, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(GPIO_TOF_8, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(GPIO_TOF_9, BCM2835_GPIO_FSEL_OUTP);
+	bcm2835_gpio_fsel(GPIO_TOF_10, BCM2835_GPIO_FSEL_OUTP);
+
+	//disable all sensors first
+	bcm2835_gpio_clr(GPIO_TOF_1); // górny
+	bcm2835_gpio_clr(GPIO_TOF_2); // górny
+	bcm2835_gpio_clr(GPIO_TOF_3);
+	bcm2835_gpio_clr(GPIO_TOF_4);
+	bcm2835_gpio_clr(GPIO_TOF_5);
+	bcm2835_gpio_clr(GPIO_TOF_6);
+	bcm2835_gpio_clr(GPIO_TOF_7);
+	bcm2835_gpio_clr(GPIO_TOF_8);
+	bcm2835_gpio_clr(GPIO_TOF_9);
+	bcm2835_gpio_clr(GPIO_TOF_10);
+
+	bcm2835_i2c_begin(); //begin I2C
+	bcm2835_i2c_set_baudrate(40000);
+
+	//enable sensor one and change address
+	bcm2835_gpio_set(GPIO_TOF_1);
+	delay(10);
+	globalSensors[0] = new VL53L1X(VL53L1X::Medium,0x29);
+	delay(10);
+	globalSensors[0]->setAddress(0x30);
+	delay(10);
+	puts("Sensor one started at: 0x30");
+
+	bcm2835_gpio_set(GPIO_TOF_2);
+	delay(10);
+	globalSensors[1] = new VL53L1X(VL53L1X::Medium,0x29);
+	delay(10);
+	globalSensors[1]->setAddress(0x31);
+	delay(10);
+	puts("Sensor two started at: 0x31");
+
+	bcm2835_gpio_set(GPIO_TOF_3);
+	delay(10);
+	globalSensors[2] = new VL53L1X(VL53L1X::Medium,0x29);
+	delay(10);
+	globalSensors[2]->setAddress(0x32);
+	delay(10);
+	puts("Sensor three started at: 0x32");
+
+	bcm2835_gpio_set(GPIO_TOF_4);
+	delay(10);
+	globalSensors[3] = new VL53L1X(VL53L1X::Medium,0x29);
+	delay(10);
+	globalSensors[3]->setAddress(0x33);
+	delay(10);
+	puts("Sensor four started at: 0x33");
+
+	bcm2835_gpio_set(GPIO_TOF_5);
+	delay(10);
+	globalSensors[4] = new VL53L1X(VL53L1X::Medium,0x29);
+	delay(10);
+	globalSensors[4]->setAddress(0x34);
+	delay(10);
+	puts("Sensor five started at: 0x34");
+
+	bcm2835_gpio_set(GPIO_TOF_6);
+	delay(10);
+	globalSensors[5] = new VL53L1X(VL53L1X::Medium,0x29);
+	delay(10);
+	globalSensors[5]->setAddress(0x35);
+	delay(10);
+	puts("Sensor six started at: 0x35");
+
+	bcm2835_gpio_set(GPIO_TOF_7);
+	delay(10);
+	globalSensors[6] = new VL53L1X(VL53L1X::Medium,0x29);
+	delay(10);
+	globalSensors[6]->setAddress(0x36);
+	delay(10);
+	puts("Sensor seven started at: 0x36");
+
+	bcm2835_gpio_set(GPIO_TOF_8);
+	delay(10);
+	globalSensors[7] = new VL53L1X(VL53L1X::Medium,0x29);
+	delay(10);
+	globalSensors[7]->setAddress(0x37);
+	delay(10);
+	puts("Sensor eight started at: 0x37");
+
+	bcm2835_gpio_set(GPIO_TOF_9);
+	delay(10);
+	globalSensors[8] = new VL53L1X(VL53L1X::Medium,0x29);
+	delay(10);
+	globalSensors[8]->setAddress(0x38);
+	delay(10);
+	puts("Sensor nine started at: 0x38");
+
+	bcm2835_gpio_set(GPIO_TOF_10);
+	delay(10);
+	globalSensors[9] = new VL53L1X(VL53L1X::Medium,0x29);
+	delay(10);
+	globalSensors[9]->setAddress(0x39);
+	delay(10);
+	puts("Sensor ten started at: 0x39");
+
+
+	for(int i=0; i<10; i++){
+		globalSensors[i]->startContinuous(20);
+		delay(10);
+	}
+
+	for(int j=0; j<1000; j++){
+	//while(1){
+		//for(int i=0; i<10; i++){
+		//			measurement[i] = globalSensors[i]->readData(1);
+		//			printf("%d:%5d ",i,measurement[i]);
+		//}
+				measurement[5] = globalSensors[5]->readData(1);
+				printf("5:%5d ",measurement[5]);
+				measurement[8] = globalSensors[8]->readData(1);
+				printf("8:%5d ",measurement[8]);
+				delay(20);
+
+		printf("\n");
+		printf("\033[H\033[J");
+	}
+
+	for(int i=0; i<10; i++){
+		globalSensors[i]->disable();
+		delay(10);
+	}
+
+	file.close();
+}
+
+void IMUtest(){
+	LSM6DS3 SensorOne( I2C_MODE, 0x6B);
+
+	if( SensorOne.begin() != 0 )
+	{
+		  printf("Problem starting the sensor \n");
+		  return;
+	}
+	else
+	{
+		  printf("Sensor with CS1 started, awaiting calibration.\n");
+		  for (int i = 10; i>0; i--){
+			  printf("%d\n",i);
+			  delay(1000);
+		  }
+	}
+	int i, n = 1000;
+
+	// Acceleration
+	float  ax, ay, az;
+	ax = SensorOne.readFloatAccelX();
+	ay = SensorOne.readFloatAccelY();
+	az = SensorOne.readFloatAccelZ();
+
+	// Gyro
+	float rgx, rgy, rgz;
+	const float offX = 3.755909, offY = -5.435584, offZ = -3.461446;
+	const float offXRad = -0.000329, offYRad = -0.000224, offZRad = 0.000880;
+	rgx = (SensorOne.readFloatGyroX() - offX)*M_PI/180 - offXRad;
+	rgy = (SensorOne.readFloatGyroY() - offY)*M_PI/180 - offYRad;
+	rgz = (SensorOne.readFloatGyroZ() - offZ)*M_PI/180 - offZRad;
+
+	// Sum of gyro readings
+	float sumgx = 0, sumgy = 0, sumgz = 0;
+
+	// Tilt
+	float ty = atan2(ax,sqrt(ay*ay+az*az));
+
+	// Filtering
+	float param = 0.1;
+	float freq = 100;
+	filter f(ty,param,freq);
+	float f_gy, f_ty;
+
+	// Log file
+	char filename [25];
+	sprintf(filename,"imu_log_report_%.3f_%04.0f", param, freq);
+	file.open(filename);
+
+	for(i=0; i<n; i++){
+		//Get all parameters
+		printf("\nAccelerometer:\n");
+		ax = SensorOne.readFloatAccelX();
+		ay = SensorOne.readFloatAccelY();
+		az = SensorOne.readFloatAccelZ();
+		printf(" X = %f\n",ax);
+		printf(" Y = %f\n",ay);
+		printf(" Z = %f\n",az);
+		printf(" ACC = %f\n", sqrt(ax*ax+ay*ay+az*az));
+
+		printf("\nGyroscope:\n");
+		rgx = (SensorOne.readFloatGyroX() - offX)*M_PI/180 - offXRad;
+		rgy = (SensorOne.readFloatGyroY() - offY)*M_PI/180 - offYRad;
+		rgz = (SensorOne.readFloatGyroZ() - offZ)*M_PI/180 - offZRad;
+		sumgx += rgx;
+		sumgy += rgy;
+		sumgz += rgz;
+		printf(" X = %f\n",rgx);
+		printf(" Y = %f",rgy);
+		printf(" Z = %f\n",rgz);
+
+		printf("\nTilt:\n");
+		ty = atan2(ax,sqrt(ay*ay+az*az));
+		printf(" Tilt = %f", ty);
+
+		printf("\nAfter filtering:\n");
+		f_ty = f.getAngle(ty,rgy);
+		f_gy = f.getGyro();
+		printf(" Gyro Y: %f", f_gy);
+		printf(" Tilt Y: %f\n", f_ty);
+
+		printf("\nThermometer:\n");
+		printf(" Degrees C = %f\n",SensorOne.readTempC());
+
+		printf("\nSensorOne Bus Errors Reported:\n");
+		printf(" All '1's = %d\n",SensorOne.allOnesCounter);
+		printf(" Non-success = %d\n",SensorOne.nonSuccessCounter);
+
+		//Write to file
+		file << i << " " << rgy << " " << ty << " " << f_gy << " " << f_ty << std::endl;
+		delay(1000/freq);
+	}
+	file.close();
+
+	printf(" Average Gyro X = %f\n", sumgx/n);
+	printf(" Average Gyro Y = %f\n", sumgy/n);
+	printf(" Average Gyro Z = %f\n", sumgz/n);
+
+	SensorOne.close_i2c();
 }
